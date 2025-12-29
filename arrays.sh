f5e965748270d900eeea0e4faba74830158ebaf7
Hemanth kumar <phemanth.kumar19@gmail.com>
2025-12-29 11:27:16 +0530

    pushing changes from datatypes file new file

--- arrays.sh
+++ arrays.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 FRUITS=("BANANA" "APPLE" "GUAVA")
-echo "First fruit name is: ${(FRUITS[0])}"
-echo "Second fruit name is: ${(FRUITS[1])}"
-echo "Third fruit name is :${(Fruits[3])}"
+echo "First fruit name is: ${FRUITS[0]}"
+echo "Second fruit name is: ${FRUITS[1]}"
+echo "Third fruit name is :${FRUITS[3]}"
