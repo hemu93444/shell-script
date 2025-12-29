cc4314f4b7426ec1e18c33de28e86cad5a861b14
Hemanth kumar <phemanth.kumar19@gmail.com>
2025-12-29 10:42:26 +0530

    pushing changes in var,arguments new file

---
+++ variables-arguments.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+PERSON1=$1
+PERSON2=$2
+echo "$PERSON1:hi $PERSON2 good day"
+echo "$PERSON2:hello $PERSON1,very good day"
+echo "$PERSON1:how are you?"
+echo "$PERSON2:I am doing good"
