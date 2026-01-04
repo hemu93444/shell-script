8edccf613f49e851e890f40e7f274438009eef7e
Hemanth kumar <phemanth.kumar19@gmail.com>
2026-01-04 10:07:37 +0530

    functions first commit

--- datatypes.sh
+++ datatypes.sh
@@ -2,4 +2,17 @@
 NUMBER1=$1
 NUMBER2=$2
 SUM=$(($NUMBER1+$NUMBER2))
-echo "Total is :: $SUM"
+echo "Total is :: $SUM"#!/bin/bash
+
+NUMBER1=$1
+NUMBER2=$2
+
+SUM=$(($NUMBER1+$NUMBER2))
+
+echo "Total:: $SUM"
+
+echo "How many args passed:: $#"
+
+echo "All args passed:: $@"
+
+echo "Script name:: $0"
